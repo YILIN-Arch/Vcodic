4da2029dd362a38cb7d6c106799f6f7be7c0453f
YILIN-Arch <lyl549439629@gmail.com>
2026-06-08 22:58:37 +0800

    Configure Supabase publishable client

--- site-config.js
+++ site-config.js
@@ -1,6 +1,6 @@
 window.VCODIC_CONFIG = Object.freeze({
-  supabaseUrl: "",
-  supabaseAnonKey: "",
+  supabaseUrl: "https://cwoivgvmjdkxzcehxppy.supabase.co",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN3b2l2Z3ZtamRreHpjZWh4cHB5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA5MDY3MDcsImV4cCI6MjA5NjQ4MjcwN30.xmNzw44kQz3MC-ijLOwdHhihhzTCN2shmThHTq11AEM",
   storageBucket: "submission-images",
   maxUploadBytes: 5 * 1024 * 1024,
 });
